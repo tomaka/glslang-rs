96f25f58d906ce0e125bf1e945887d30c908edb8
Pierre Krieger <pierre.krieger1708@gmail.com>
2015-01-19 15:25:49 +0100

    Provide binaries instead of using hyper

--- build.rs
+++ build.rs
@@ -1,25 +1,18 @@
-extern crate hyper;
-
 use std::os;
-use std::io::fs::File;
-use hyper::Client;
 
 fn main() {
     let target = os::getenv("TARGET").unwrap();
     let out_file = Path::new(os::getenv("OUT_DIR").unwrap()).join("glslang_validator");
 
-    let url = if target.contains("windows") {
-        "https://cvs.khronos.org/svn/repos/ogl/trunk/ecosystem/public/sdk/tools/glslang/Install/Windows/glslangValidator.exe"
+    let path = if target.contains("windows") {
+        Path::new("bin/glslangValidator.exe")
     } else if target.contains("linux") {
-        "https://cvs.khronos.org/svn/repos/ogl/trunk/ecosystem/public/sdk/tools/glslang/Install/Linux/glslangValidator"
+        Path::new("bin/glslangValidator")
     } else {
         panic!("The platform `{}` is not supported", target);
     };
 
-    let mut client = Client::new();
-
-    let mut res = client.get(url).send().unwrap();
-    assert_eq!(res.status, hyper::Ok);
-
-    File::create(&out_file).unwrap().write(res.read_to_end().unwrap().as_slice()).unwrap();
+    if let Err(_) = std::io::fs::link(&path, &out_file) {
+        std::io::fs::copy(&path, &out_file).unwrap();
+    }
 }
